5edb1ebf1a26f59176e159ac18dc9054633402eb
Hok Vicheka <hvicheka.it@gmail.com>
2022-03-17 22:54:54 +0700

    init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,77 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Awesome Todo
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Navigation
+        </q-item-label>
+        <SideBar
+          v-for="link in sideBarLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import SideBar from 'components/Sidebar.vue'
+
+const linksData = [
+  {
+    title: 'Todo',
+    icon: 'list',
+    link: '/'
+  },
+  {
+    title: 'Settings',
+    icon: 'settings',
+    link: '/settings'
+  }
+];
+
+export default {
+  name: 'MainLayout',
+  components: {
+    SideBar
+  },
+  data() {
+    return {
+      leftDrawerOpen: false,
+      sideBarLinks: linksData
+    }
+  }
+}
+</script>
